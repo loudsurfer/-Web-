c67b6dcc06fc631d4a90fed91bcda49671200570
loudsurfer <user@MacBook-Pro-User.local>
2020-12-07 14:57:52 +0300

    upload

---
+++ Лабораторные работы/webLabs/Lab1/mygroup.py
@@ -0,0 +1,36 @@
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 4]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [5, 5, 5]
+    }
+]
+
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+
+def print_average_score(marks):
+    return 0 if not marks else sum(marks) / len(marks)
+
+def print_average_score_print_students(students, average_score):
+    for student in students:
+        if print_average_score(student["marks"]) > average_score:
+            print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30), str(student["marks"]).ljust(20))
+
+print_average_score_print_students(groupmates, 2)
+
